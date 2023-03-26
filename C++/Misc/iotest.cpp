e8e1df321d16b36509c337174a9f20715b8e60e7
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-03-26 13:40:07 +0000

    update *

---
+++ C++/Misc/iotest.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+    string e;
+    printf("Hello: ");
+    cin >> e;
+    cout << "Important\n";
+    cout << e << "\n";
+}
