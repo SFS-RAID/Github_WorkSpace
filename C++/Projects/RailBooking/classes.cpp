a6d7a60ca0b8f327f68b61d7d7257216b9cdb075
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-07-02 03:57:06 +0000

    RailBooking FW Done

--- C++/Projects/RailBooking/classes.cpp
+++ C++/Projects/RailBooking/classes.cpp
@@ -7,23 +7,46 @@ private:
     /* data */
 public:
     string name;
-    station(string name){
-        this->name = name;
+    string division="";
+
+    station(string name, string div="NA")
+        : name(name), division(div)
+        /*Initalising, outvariable(parameter)=
+        this->outvariable = parameter*/
+    {
     }
 };
 
+station defaultstn("default");
+station defaultstn2("default2");
+
+class route{
+public:
+    station& start;
+    station& end;
+    //Declare station variable for object
+    int distance;
+
+    route(station& startpoint, station& endpoint, int distance=100)
+        : start(startpoint), end(endpoint), distance(distance)
+    {
+    }
+};
+
+route defaultroute(defaultstn, defaultstn2);
 
 class train
 {
 private:
     /* data */
 public:
-    int train_num;
-    string name, source, destination;
-    train(int train_no, string train_name, station& start, station& end){
-        this->train_num=train_no;
-        this->name = train_name;
-        this->source = start.name;
-        this->destination = end.name;
+    int train_num, distance=0;
+    string name;
+    route& route_train;
+
+    train(int train_no, string train_name, route& trainroute)
+        : train_num(train_no), name(train_name), route_train(trainroute)
+    {
+        distance = route_train.distance;
     }
 };
