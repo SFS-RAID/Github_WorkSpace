183182a2259a362d911ecbcf72647bae395109e8
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-07-23 14:35:16 +0530

    Update classes.cpp

--- C++/Projects/RailBooking/classes.cpp
+++ C++/Projects/RailBooking/classes.cpp
@@ -1,52 +1,23 @@
-#include <string>
-using namespace std;
+#include "classes.h"
+#include <cmath>
 
-class station
-{
-private:
-    /* data */
-public:
-    string name;
-    string division="";
+Station::Station(const std::string& name, const std::string& div, int livetracks_near,
+    int livetracks_far, int yardtracks, int platform_count)
+    : name(name), division(div), livetr_near(livetracks_near),
+    livetr_far(livetracks_near), yard_tracks(yardtracks), platforms(platform_count) {
+}
 
-    station(string name, string div="NA")
-        : name(name), division(div)
-        /*Initalising, outvariable(parameter)=
-        this->outvariable = parameter*/
-    {
-    }
-};
+Station defaultstn("default");
+Station defaultstn2("default2");
 
-station defaultstn("default");
-station defaultstn2("default2");
+Route::Route(Station& startpoint, Station& endpoint, int distance)
+    : start(startpoint), end(endpoint), distance(distance) {
+}
 
-class route{
-public:
-    station& start;
-    station& end;
-    //Declare station variable for object
-    int distance;
+Route defaultroute(defaultstn, defaultstn2);
 
-    route(station& startpoint, station& endpoint, int distance=100)
-        : start(startpoint), end(endpoint), distance(distance)
-    {
-    }
-};
-
-route defaultroute(defaultstn, defaultstn2);
-
-class train
-{
-private:
-    /* data */
-public:
-    int train_num, distance=0;
-    string name;
-    route& route_train;
-
-    train(int train_no, string train_name, route& trainroute)
-        : train_num(train_no), name(train_name), route_train(trainroute)
-    {
-        distance = route_train.distance;
-    }
-};
+Train::Train(int train_no, const std::string& train_name, Route& trainroute, int cost_perkm)
+    : train_num(train_no), name(train_name), route_train(trainroute) {
+    distance = route_train.distance;
+    cost = round(distance * cost_perkm);
+}
