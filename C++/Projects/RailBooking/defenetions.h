6871f93da2ba8eec68cdd4083edcbf03a1f2b00c
SFS-RAID <99122481+SFS-RAID@users.noreply.github.com>
2023-07-23 14:32:19 +0530

    Create defenetions.h

---
+++ C++/Projects/RailBooking/defenetions.h
@@ -0,0 +1,10 @@
+#ifndef DEFENITIONS_H
+#define DEFENITIONS_H
+
+#include <string>
+
+constexpr unsigned int str2int(const char* str, int h = 0);
+
+std::string input(const std::string& text);
+
+#endif // DEFENITIONS_H
